7b9285f44dad9fa67571c0073ef1ea9d8d5736c3
Dluter <tom230023@gmail.com>
2023-08-24 09:31:46 +0800

    :triangular_flag_on_post: add developer get

--- apps/api/src/developer/developer.controller.spec.ts
+++ apps/api/src/developer/developer.controller.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DeveloperController } from './developer.controller';
+import { DeveloperService } from './developer.service';
 
 describe('DeveloperController', () => {
   let controller: DeveloperController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [DeveloperService],
       controllers: [DeveloperController],
     }).compile();
 
--- apps/api/src/developer/developer.controller.ts
+++ apps/api/src/developer/developer.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { DeveloperService } from './developer.service';
 
 @Controller('developer')
 export class DeveloperController {
   constructor(private developerService: DeveloperService) {}
+
+  @Get()
+  index() {
+    return 'hello, this is developer api';
+  }
 }
