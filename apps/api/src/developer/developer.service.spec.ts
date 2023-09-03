b12542fa905ce08ddec5e75f9a7690122388d400
Dluter <tom230023@gmail.com>
2023-09-04 01:13:29 +0800

    :pencil2: fix grammar

--- apps/api/src/developer/developer.service.spec.ts
+++ apps/api/src/developer/developer.service.spec.ts
@@ -20,43 +20,43 @@ describe('DeveloperService', () => {
     expectTypeOf(service.getDevelopers()).toBeArray();
   });
 
-  it('Developers should be has Dlutermade', () => {
+  it('Developers should have Dlutermade', () => {
     expect(service.getDevelopers()).toEqual(
       expect.arrayContaining(['Dlutermade']),
     );
   });
 
-  it('Developers should be has leave3310', () => {
+  it('Developers should have leave3310', () => {
     expect(service.getDevelopers()).toEqual(
       expect.arrayContaining(['leave3310']),
     );
   });
 
-  it('Developers should be has Pikacnu', () => {
+  it('Developers should have Pikacnu', () => {
     expect(service.getDevelopers()).toEqual(
       expect.arrayContaining(['Pikacnu']),
     );
   });
 
-  it('Developers should be has miku3920', () => {
+  it('Developers should havemiku3920', () => {
     expect(service.getDevelopers()).toEqual(
       expect.arrayContaining(['miku3920']),
     );
   });
 
-  it('Developers should be has laihongde', () => {
+  it('Developers should have laihongde', () => {
     expect(service.getDevelopers()).toEqual(
       expect.arrayContaining(['laihongde']),
     );
   });
 
-  it('Developers should be has Tuhacrt', () => {
+  it('Developers should have Tuhacrt', () => {
     expect(service.getDevelopers()).toEqual(
       expect.arrayContaining(['Tuhacrt']),
     );
   });
 
-  it('Developers should be has yockwang', () => {
+  it('Developers should have yockwang', () => {
     expect(service.getDevelopers()).toEqual(
       expect.arrayContaining(['yockwang']),
     );
