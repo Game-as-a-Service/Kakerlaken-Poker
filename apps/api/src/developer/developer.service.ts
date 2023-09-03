1a9c817ea6e5b38ec4a85cd95c2a6c7ee1a6d01f
Dluter <tom230023@gmail.com>
2023-09-03 22:07:08 +0800

    :beers: add developer

--- apps/api/src/developer/developer.service.ts
+++ apps/api/src/developer/developer.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class DeveloperService {}
+export class DeveloperService {
+  getDevelopers() {
+    return ['Dlutermade'];
+  }
+}
