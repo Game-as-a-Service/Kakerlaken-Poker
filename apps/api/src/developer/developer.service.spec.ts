0943e1696a2a0f6f9e1a994560b010249dad50e8
Dluter <tom230023@gmail.com>
2023-09-18 06:36:43 +0800

    test

--- apps/api/src/developer/developer.service.spec.ts
+++ apps/api/src/developer/developer.service.spec.ts
@@ -17,7 +17,7 @@ describe('DeveloperService', () => {
   });
 
   it('Developers should be array', () => {
-    expect(Array.isArray(service.getDevelopers())).toBe(false);
+    expect(Array.isArray(service.getDevelopers())).toBe(true);
   });
 
   it('Developers should have Dlutermade', () => {
