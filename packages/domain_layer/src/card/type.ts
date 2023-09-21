b63bd8cca21c18a99cc5b2260a5514e633b5a3b9
BillMo <tom230023@gmail.com>
2023-09-21 19:19:16 +0800

    :label: add card base type (#40)
    * :label: add card base type
    
    * :label: add creatures

---
+++ packages/domain_layer/src/card/type.ts
@@ -0,0 +1,21 @@
+export enum Creatures {
+  Bat = 'BAT',
+  Cockroach = 'COCKROACH',
+  Fly = 'FLY',
+  Toad = 'TOAD',
+  Rat = 'RAT',
+  Scorpion = 'SCORPION',
+  Spider = 'SPIDER',
+  StickBug = 'STICK_BUG',
+}
+
+export enum Status {
+  Unrevealed = 'UNREVEALED',
+  Revealed = 'REVEALED',
+}
+
+export type Card = {
+  id: string;
+  status: Status;
+  creature: Creatures;
+};
--- packages/domain_layer/src/index.ts
+++ packages/domain_layer/src/index.ts
@@ -1,3 +1,4 @@
 export * as game from './game';
 export * as round from './round';
 export * as player from './player';
+export * as card from './card';
