c4bf89aec0b278960985d7a127818883b70a8199
Tuhacrt <a067800497@gmail.com>
2023-09-23 11:19:34 +0800

    :wrench: add eslint-plugin-import to lint import

--- packages/domain_layer/.eslintrc.js
+++ packages/domain_layer/.eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'import'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
@@ -22,5 +22,20 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'unknown',
+        ],
+        'newlines-between': 'always',
+      },
+    ],
   },
 };
